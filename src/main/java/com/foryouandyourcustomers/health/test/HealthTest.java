922b896f2d08b284edb2dad272ba00ce0db0a945
Federico <fes@foryouandyourcustomers.com>
2017-11-29 11:15:23 +0100

    Fixing the release to publish

--- src/main/java/com/foryouandyourcustomers/health/test/HealthTest.java
+++ src/main/java/com/foryouandyourcustomers/health/test/HealthTest.java
@@ -5,9 +5,6 @@ import static com.foryouandyourcustomers.health.HealthCheckType.MEDIUM;
 import static com.foryouandyourcustomers.health.HealthCheckType.ONCE;
 import static com.foryouandyourcustomers.health.HealthCheckType.SHORT;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import com.foryouandyourcustomers.health.HealthCheck;
 import com.google.common.collect.Maps;
@@ -19,12 +16,8 @@ public class HealthTest {
   private Map<String, Boolean> ranTest = Maps.newHashMap();
 
   @HealthCheck(type = ONCE)
-  public Map<String, List<String>> onceCheck() {
+  public void onceCheck() {
     this.ranTest.put(ONCE.toString(), true);
-    HashMap<String, List<String>> objectObjectHashMap = Maps.newHashMap();
-    objectObjectHashMap.put("pedro", Arrays.asList("es un quejica del código", "sisisisi"));
-    objectObjectHashMap.put("fede", Arrays.asList("es un desastre  del código"));
-    return objectObjectHashMap;
   }
 
   @HealthCheck(type = SHORT)
